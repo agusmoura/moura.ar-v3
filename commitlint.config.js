370a4d67dd302f83eeb71c913023484a9f94ecbe
Agustin Mouriño - Personal <agus442m@gmail.com>
2025-05-29 15:17:08 -0300

    chore: increase header max length from 72 to 80 characters in commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -22,6 +22,6 @@ export default {
     'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
-    'header-max-length': [2, 'always', 72],
+    'header-max-length': [2, 'always', 80],
   },
 };
