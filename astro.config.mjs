a4623256546e2f6b2d190da27b0e481c6bfbdecc
Agustin Mouriño - Personal <agus442m@gmail.com>
2025-06-04 20:43:41 -0300

    feat: integrate astro-icon for tech stack visualization and add Proyects section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [icon()],
   site: 'https://moura.ar', // Cambia esto a tu dominio real
   compressHTML: true,
   build: {
